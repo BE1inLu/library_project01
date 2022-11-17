5aae60a23f8a1e1e4974c357d32486ec16f5302a
TeufulNeko <enluba2333@gmail.com>
2022-11-17 14:54:45 +0800

    base

---
+++ src/main/java/com/book/book.java
@@ -0,0 +1,66 @@
+package com.book;
+
+
+public class book {
+    
+    int bookid=0;
+    String bookname=null;
+    int borrow_num=0;
+    int receive_num=0;
+    int depot=0;
+
+    public book(){
+        super();
+    }
+
+    public book(int bookid,String bookname,int borrow_num,int receive_num,int depot){
+        this.bookid=bookid;
+        this.bookname=bookname;
+        this.borrow_num=borrow_num;
+        this.receive_num=receive_num;
+        this.depot=depot;
+    }
+
+    public int getBookid() {
+        return bookid;
+    }
+    public void setBookid(int bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getBookname() {
+        return bookname;
+    }
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public int getBorrow_num() {
+        return borrow_num;
+    }
+    public void setBorrow_num(int borrow_num) {
+        this.borrow_num = borrow_num;
+    }
+
+    public int getReceive_num() {
+        return receive_num;
+    }
+    public void setReceive_num(int receive_num) {
+        this.receive_num = receive_num;
+    }
+
+    public int getDepot() {
+        return depot;
+    }
+    public void setDepot(int depot) {
+        this.depot = depot;
+    }
+
+    @Override
+    public String toString() {
+        return "bookid="+bookid+",bookname="+bookname+",=borrow_num="+borrow_num+",receive_num="+receive_num+",depot="+depot;
+    }
+
+
+    
+}
--- src/main/java/com/dao/dao.java
+++ src/main/java/com/dao/dao.java
@@ -2,27 +2,28 @@ package com.dao;
 
 import java.sql.*;
 import com.user.*;
+import com.book.*;
 // import com.sql.JDBCUtil;
 
 public class dao {
 
     // user数据库操作--登录模块
-    public user login(Connection conn,user user) throws Exception{
+    public user login(Connection conn, user user) throws Exception {
 
-        user resultUser=null;
+        user resultUser = null;
 
-        //数据库指令
-        String sql="select * from user where username=? and password=?";
+        // 数据库指令
+        String sql = "select * from user where username=? and password=?";
 
-        //获得对象
-        PreparedStatement pst=conn.prepareStatement(sql);
+        // 获得对象
+        PreparedStatement pst = conn.prepareStatement(sql);
         pst.setString(1, user.getUsername());
         pst.setString(2, user.getPassword());
 
-        //获得结果
-        ResultSet rs=pst.executeQuery();
-        if(rs.next()){
-            resultUser =new user();
+        // 获得结果
+        ResultSet rs = pst.executeQuery();
+        if (rs.next()) {
+            resultUser = new user();
             resultUser.setUsername(rs.getString("username"));
             resultUser.setPassword(rs.getString("password"));
         }
@@ -30,17 +31,17 @@ public class dao {
         return resultUser;
     }
 
-    //user数据库操作--注册模块
-    public boolean regist(Connection conn,user user)throws Exception{
-        
-        //定义一个布尔类，用于返回操作结果
-        boolean flag=false;
+    // user数据库操作--注册模块
+    public boolean regist(Connection conn, user user) throws Exception {
 
-        //sql指令操作
-        String sql="insert into user(username,password,mail,tel,sex) VALUES(?,?,?,?,?)";
-        PreparedStatement pst=conn.prepareStatement(sql);
+        // 定义一个布尔类，用于返回操作结果
+        boolean flag = false;
 
-        //接收前台回传的数据，匹配指令val
+        // sql指令操作
+        String sql = "insert into user(username,password,mail,tel,sex) VALUES(?,?,?,?,?)";
+        PreparedStatement pst = conn.prepareStatement(sql);
+
+        // 接收前台回传的数据，匹配指令val
         pst.setString(1, user.getUsername());
         pst.setString(2, user.getPassword());
         pst.setString(3, user.getEmail());
@@ -48,11 +49,30 @@ public class dao {
         pst.setString(5, user.getSex());
 
         // 执行指令，返回int数值用来表示注册是否成功
-        int res=pst.executeUpdate();
-        if(res>0) flag=true;
+        int res = pst.executeUpdate();
+        if (res > 0)
+            flag = true;
         return flag;
     }
 
-    // TODO ：user数据库操作，回传user内容
-    
+    // TODO：user数据库操作，回传user数据库信息
+
+    // TODO：book数据库操作，展示书籍列表
+
+    // TODO：book数据库操作，接收书籍名，回传书籍借阅情况
+    public book[] getBook(Connection conn, book book) throws Exception {
+        book newbook[] = null;
+        String sql = "select * from book where bookname=?";
+
+        PreparedStatement pst = conn.prepareStatement(sql);
+        pst.setString(1, book.getBookname());
+
+        ResultSet rs = pst.executeQuery();
+        while (rs.next()) {
+
+        }
+
+        return newbook;
+    }
+
 }
